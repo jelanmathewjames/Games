e7b7213ebe783e66c4b906a4d4c51647623234f7
jelanmathewjames <jelanmathewjames1234@gmail.com>
2023-01-26 12:31:36 -0600

    setting win condition

--- static/newtictactoe_play.js
+++ static/newtictactoe_play.js
@@ -13,6 +13,31 @@ chatSocket.onopen = (e)=>{
         })
     )
 }
+function checkWinner(){
+    if(icons[0]==icons[1] && icons[0]==icons[2] && icons[0]!='+'){
+        winningChanges()
+    }else if(icons[3]==icons[4]&& icons[3]==icons[5] && icons[3]!='+'){
+        winningChanges()
+    }else if(icons[6]==icons[7] && icons[6]==icons[8] && icons[6]!='+'){
+        winningChanges()
+    }else if(icons[0]==icons[4] && icons[0]==icons[8] && icons[0]!='+'){
+        winningChanges()
+    }else if(icons[2]==icons[4] && icons[2]==icons[6] && icons[2]!='+'){
+        winningChanges()
+    }else if(icons[0]==icons[3] && icons[0]==icons[6] && icons[0]!='+'){
+        winningChanges()
+    }else if(icons[1]==icons[4] && icons[1]==icons[7] && icons[1]!='+'){
+        winningChanges()
+    }else if(icons[2]==icons[5] && icons[2]==icons[8] && icons[2]!='+'){
+        winningChanges()
+    }
+}
+function winningChanges(){
+    chatSocket.send(
+        JSON.stringify({
+            type : "win_condition"
+        }))
+}
 function buttonAction(button_num){
     if (count < 6){
         if(icons[button_num] == '+'){
@@ -58,7 +83,7 @@ function buttonAction(button_num){
                 }else if(move ==8 && (button_num == 5 || button_num == 7 || button_num == 4) ){
                     success = true
                 }else{
-                    alert("Cannot move to that Position")
+                  alert("Cannot move to that Position")
                 }
                 if(success){
                     chatSocket.send(JSON.stringify({
@@ -87,7 +112,9 @@ function reset_game(){
 }
 chatSocket.onmessage = (e)=>{
     const data = JSON.parse(e.data)
-    if(data.type == 'movement_after_setting'){
+    if(data.type == 'win_condition'){
+        alert("Winned condition")
+    }else if(data.type == 'movement_after_setting'){
         if(data.turn == 'your'){
             document.getElementById('button'+data.position_icon).className = "btn btn-success"
         }else if(data.turn == 'opponent'){
@@ -99,6 +126,7 @@ chatSocket.onmessage = (e)=>{
         document.getElementById('button'+data.move_icon).className = "btn btn-dark"
         document.getElementById('button'+data.position_icon).innerHTML = data.button
         document.getElementById('button'+data.move_icon).innerHTML = '+'
+        checkWinner()
     }
     else if(data.type == 'check_turn'){
         if(data.is_your_turn){
@@ -121,6 +149,7 @@ chatSocket.onmessage = (e)=>{
         icons[data.button] = data.player_side
         count++
         count >= 6?alert("You have fixed your icons. Move it"):console.log("not fixed")
+        checkWinner()
     }else if(data.type == 'not_your_turn'){
         alert("Not your turn")
     }else if(data.type == 'reload_after_game_on'){
@@ -212,102 +241,3 @@ $("#btnO").click(()=>{
     }
     
 })
-/*let icons = ["+","+","+","+","+","+","+","+","+"]
-let count = 0
-let move = null
-let success = true
-
-chatSocket.onmessage = (e)=>{
-    const data = JSON.parse(e.data)
-    const command = data.command
-    if(command == 'playermove'){
-        if(command.success == 'true')
-            changePosition(move,position)
-    }
-        
-}
-function changePosition(move,position){
-    [icons[position],icons[move]] = [icons[move],icons[position]]
-    document.getElementById("button"+move).innerHTML = "+"
-    document.getElementById("button"+position).innerHTML = current_player
-    success = true
-}
-function playerMove(position){
-    if(count<6){
-        if(icons[position] == "+"){
-            icons[position] = current_player
-            document.getElementById("button"+position).innerHTML = current_player
-            count++
-            success = true
-        }else{
-            alert("Wrong entry")
-            success = false
-        }
-    }else{
-        if(move == null){
-            if(icons[position] == current_player){
-                    move = position
-                }else{
-                    alert("Not your Icon")
-                }
-                success = false
-            }else if(move != null){
-                if(icons[position] == "+"){
-                    if(move == 0 &&  (position == 1 || position == 3 || position == 4)){
-                        changePosition(move,position)
-                    }else if(move == 1 && (position == 0 || position == 2 || position == 4) ){
-                        changePosition(move,position)
-                    }else if(move ==2 && (position == 1 || position == 5 || position == 4) ){
-                        changePosition(move,position)
-                    }else if(move ==3 && (position == 4 || position == 0 || position == 6) ){
-                        changePosition(move,position)
-                    }else if(move ==4){
-                        changePosition(move,position)
-                    }else if(move ==5 && (position == 2 || position == 8 || position == 4)){
-                        changePosition(move,position)
-                    }else if(move ==6 && (position == 3 || position == 7 || position == 4) ){
-                        changePosition(move,position)
-                    }else if(move ==7 && (position == 6 || position == 8 || position == 4) ){
-                        changePosition(move,position)
-                    }else if(move ==8 && (position == 5 || position == 7 || position == 4) ){
-                        changePosition(move,position)
-                    }else{
-                        alert("Cannot move to that Position")
-                        success = false
-                    }
-                }else{
-                    alert("Already occupied")
-                    success = false
-                }
-                move = null
-            }
-    }
-        
-}
-function checkWinner(){
-    if(icons[0]==icons[1] && icons[0]==icons[2] && icons[0]!='+'){
-        winningChanges()
-    }else if(icons[3]==icons[4]&& icons[3]==icons[5] && icons[3]!='+'){
-        winningChanges()
-    }else if(icons[6]==icons[7] && icons[6]==icons[8] && icons[6]!='+'){
-        winningChanges()
-    }else if(icons[0]==icons[4] && icons[0]==icons[8] && icons[0]!='+'){
-        winningChanges()
-    }else if(icons[2]==icons[4] && icons[2]==icons[6] && icons[2]!='+'){
-        winningChanges()
-    }else if(icons[0]==icons[3] && icons[0]==icons[6] && icons[0]!='+'){
-        winningChanges()
-    }else if(icons[1]==icons[4] && icons[1]==icons[7] && icons[1]!='+'){
-        winningChanges()
-    }else if(icons[2]==icons[5] && icons[2]==icons[8] && icons[2]!='+'){
-        winningChanges()
-    }
-}
-
-function buttonAction(button_num){
-    playerMove(button_num)
-    if(success){
-        checkWinner()
-        changePlayer()
-    }
-}*/
